5b9345eb0e9a4eb34749baab0ac87be289d154ea
JS <153196703+JSe04022630047@users.noreply.github.com>
2024-07-12 10:26:13 +0700

    lab 2-2

---
+++ 66-040226-30047_Lab2-2/Lab2-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumber1;
+            int nNumber2;
+            int nSum;
+
+            Console.Write("Enter first integer: ");
+            nNumber1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            nNumber2 = int.Parse(Console.ReadLine());
+
+            nSum = nNumber1 + nNumber2;
+
+            Console.WriteLine(String.Format("Sum is {0}", nSum));
+
+            Console.ReadLine();
+        }
+    }
+}
