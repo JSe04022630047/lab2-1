dd25ebebf4edb5962ef2bc2dbcc3cea7fd049155
JS <153196703+JSe04022630047@users.noreply.github.com>
2024-09-27 17:44:20 +0700

    lab6 part 1
    rename

---
+++ Lab6/Lab6-4/BasePlusCommissionEmployeeTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_4
+{
+    class BasePlusCommissionEmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            // instantiate BasePlusCommissionEmployee object
+            var employee = new BasePlusCommissionEmployee("Man", "Lewis",
+               "333-33-3333", 5000.00M, .04M, 300.00M);
+
+            // display BasePlusCommissionEmployee's data
+            Console.WriteLine(
+               "Employee information obtained by properties and methods: \n");
+            Console.WriteLine($"First name is {employee.FirstName}");
+            Console.WriteLine($"Last name is {employee.LastName}");
+            Console.WriteLine(
+               $"Social security number is {employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales are {employee.GrossSales:C}");
+            Console.WriteLine(
+               $"Commission rate is {employee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are {employee.Earnings():C}");
+            Console.WriteLine($"Base salary is {employee.BaseSalary:C}");
+
+            employee.BaseSalary = 1000.00M; // set base salary
+
+            Console.WriteLine(
+               "\nUpdated employee information obtained by ToString:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"earnings: {employee.Earnings():C}");
+
+            Console.ReadLine();
+        }
+    }
+}
